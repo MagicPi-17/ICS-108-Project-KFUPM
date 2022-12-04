53114354998b6f57c5241110cf5a01c2e1e5806a
MagicPi-17 <9martada9@gmail.com>
2022-12-04 21:23:53 +0300

    update the course Class

--- Course.java
+++ Course.java
@@ -1,6 +1,14 @@
-class Coures {
-    String Prerequisite;
-    String Corequisite;
+class Course {
+    private String prerequisite;
+    private String corequisite;
+
+    public String getPrerequisite() {return prerequisite;}
+    public String getCorequisite() {return corequisite;}
+    
+    Course(String prerequisite, String corequisite) {
+        this.prerequisite = prerequisite;
+        this.corequisite = corequisite; 
+    }
 }
 
 
