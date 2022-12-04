7957e632ddf0a61027813dee41b4ce723086c413
MagicPi-17 <9martada9@gmail.com>
2022-12-04 22:08:25 +0300

    making changes in the Student class

--- Student.java
+++ Student.java
@@ -2,7 +2,21 @@ import java.util.ArrayList;
 
 class Student {
     private ArrayList<FinishedCourse> finishedCourses;
+
     public ArrayList<FinishedCourse> getFinishedCourses() { return finishedCourses;}
+    public void setFinishedCourses(ArrayList<FinishedCourse> finishedCourses) { this.finishedCourses = finishedCourses;}
+
+    public void addFinishedCourse(String course, String term, String grade) {
+        this.finishedCourses.add(new FinishedCourse(course, term, grade));
+    }
+
+    public void removeFinishedCourse(String course, String term, String grade) {
+        this.finishedCourses.remove(new FinishedCourse(course, term, grade));
+    }
+    public void removeFinishedCourse(int index) {
+        this.finishedCourses.remove(index);
+    }
+
     Student() {
         this.finishedCourses = new ArrayList<FinishedCourse>();
     }
