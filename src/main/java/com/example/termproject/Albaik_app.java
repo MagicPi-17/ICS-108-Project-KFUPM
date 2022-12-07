e651d02dbc104a6cfff14a243c27de1390e48f99
MagicPi-17 <9martada9@gmail.com>
2022-12-07 21:45:49 +0300

    javafx setup

--- src/main/java/com/example/termproject/Albaik_app.java
+++ src/main/java/com/example/termproject/Albaik_app.java
@@ -6,10 +6,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Albaik_app extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Albaik_app.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
