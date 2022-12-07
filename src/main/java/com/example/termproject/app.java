dfbbed7b15c506b3c552c66eb2284cd2e399d3f7
Ali Al Sules <3lims96@gmail.com>
2022-12-07 21:47:58 +0300

    getDegreePlanCoursesPane function added

--- src/main/java/com/example/termproject/app.java
+++ src/main/java/com/example/termproject/app.java
@@ -2,8 +2,16 @@ package com.example.termproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,20 +26,49 @@ public class app extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        CSVReader finishedCourses = new CSVReader("src/main/java/com/example/termproject/data/FinishedCourses.csv");
-        try {
-            String[][] degreePlanDataArray = finishedCourses.readTo2DArray();
-            for (String[] row : degreePlanDataArray) {
-                for (String ele : row) { System.out.print(ele + " ");}
-                System.out.println();
-            }
-        } catch (Exception e) {
-            System.out.println("File not found");;
-        }
-        Pane pane = new Pane();
-        Scene scene = new Scene(pane, 1500, 790);
+        HBox mainPane = new HBox(); // Used this pane to center the content horizantilly
+        mainPane.setAlignment(Pos.BASELINE_CENTER);
+
+        Scene scene = new Scene(mainPane, 1400, 740);
         stage.setTitle("Term_Project");
         stage.setScene(scene);
         stage.show();
+
+        try {
+            VBox degreePlanCoursesPane = getdegreePlanCoursesPane();
+            mainPane.getChildren().add(degreePlanCoursesPane); //adding the plan courses list to the main pane
+        }catch (IOException e) {
+            System.out.println("DegreePlan File not Found");
+        }
+    }
+
+
+    public VBox getdegreePlanCoursesPane() throws IOException{
+        CSVReader finishedCourses = new CSVReader("src/main/java/com/example/termproject/data/DegreePlan.csv");
+        String[][] degreePlanDataArray = finishedCourses.readTo2DArray();
+        GridPane planCoursesPane = new GridPane();
+        for (int i = 0; i < degreePlanDataArray.length; i++) {
+            for (int j = 0; j < degreePlanDataArray[0].length; j++) {
+                Label courseInfo = new Label(degreePlanDataArray[i][j] + "             ");
+                courseInfo.setFont(Font.font ("Verdana", 16));
+                courseInfo.setPadding(new Insets(10, 0, 0, 0));
+                planCoursesPane.add(courseInfo, j, i);
+            }
+        }
+        
+        VBox pane = new VBox(); //The pane is placing its children panes vertically
+        pane.setSpacing(50);
+
+        HBox degreePlanTitlePane = new HBox(); //Title
+        degreePlanTitlePane.setAlignment(Pos.BASELINE_CENTER);
+        degreePlanTitlePane.setPadding(new Insets(50,0,30,0));
+        Label degreePlanTitle = new Label("Degree Plan Courses");
+        degreePlanTitle.setFont(Font.font ("Verdana", 20));
+        degreePlanTitlePane.getChildren().add(degreePlanTitle);
+
+        pane.getChildren().add(degreePlanTitlePane); //adding the title to the pane 
+        pane.getChildren().add(planCoursesPane); //adding the list of plan courses to the pane
+
+        return pane;
     }
 }
