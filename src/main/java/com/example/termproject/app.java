306c441d1bf42678e1a1e4efbf3ecbd2adef4752
Ali Al Sules <3lims96@gmail.com>
2022-12-11 20:58:34 +0300

    Buttons added to course offering

--- src/main/java/com/example/termproject/app.java
+++ src/main/java/com/example/termproject/app.java
@@ -9,19 +9,26 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import com.example.termproject.classes.CSVReader;
+import com.example.termproject.classes.Section;
 
 public class app extends Application {
+    public ArrayList<Button> addButtons = new ArrayList<>();
+    public ArrayList<Button> removeButtons = new ArrayList<>();
+
 
     public static void main(String[] args) {
         launch(args);
@@ -38,67 +45,99 @@ public class app extends Application {
         stage.show();
 
         try {
-            VBox degreePlanCoursesPane = getDegreePlanCoursesPane();
+            VBox degreePlanCoursesPane = getDataTitleInPane("src/main/java/com/example/termproject/data/CourseOffering.csv", "Course Offering");
             mainPane.getChildren().add(degreePlanCoursesPane); //adding the plan courses list to the main pane
         }catch (IOException e) {
-            System.out.println("DegreegPlan File not Found");
+            System.out.println("File not Found");
         }     
     }
 
     public GridPane getDataInGridPane(String filePath) throws IOException{ //This method get the data and put them in a gridPane
-        CSVReader finishedCourses = new CSVReader(filePath);
-        String[][] degreePlanDataArray = finishedCourses.readTo2DArray();
+        CSVReader originalData = new CSVReader(filePath);
+        String[][] dataArray = originalData.readTo2DArray();
         GridPane grid = new GridPane();
-        for (int i = 0; i < degreePlanDataArray.length; i++) {
-            for (int j = 0; j < degreePlanDataArray[0].length; j++) {
-                Label info = new Label(degreePlanDataArray[i][j] + "             ");
-                info.setFont(Font.font ("Verdana", 14));
-                info.setPadding(new Insets(10, 0, 0, 0));
+        for (int i = 0; i < dataArray.length; i++) {
+            for (int j = 0; j < dataArray[i].length; j++) {
+                Label info = new Label(dataArray[i][j] + "          ");
+                info.setFont(Font.font ("Verdana", 12));
+                info.setPadding(new Insets(15, 0, 0, 0));
                 HBox infoPane = new HBox(); //To the data in their boxes
                 infoPane.getChildren().add(info);
                 infoPane.setAlignment(Pos.BASELINE_CENTER);
                 grid.add(infoPane, j, i);
+            }
+            //Adding buttons at the end
+            if (i > 0) {
+                Button addBtn = new Button("Add"); 
+                addBtn.setPadding(new Insets(15, 15, 15, 15));
+                addBtn.setId(Integer.toString(i)); //Give each button its id
+                AddHandlerClass handler1 = new AddHandlerClass();
+                addBtn.setOnAction(handler1);
+                grid.add(addBtn, dataArray[i].length, i);
+                addButtons.add(addBtn); //Adding the button to the public list of buttons
+
+                Button removeBtn = new Button("Remove"); 
+                removeBtn.setPadding(new Insets(15, 15, 15, 15));
+                removeBtn.setId(Integer.toString(i)); //Give each button its id
+                RemoveHandlerClass handler2 = new RemoveHandlerClass();
+                removeBtn.setOnAction(handler2);
+                removeBtn.setDisable(true);
+                grid.add(removeBtn, dataArray[i].length + 2, i);
+                removeButtons.add(removeBtn); //Adding the button to the public list of buttons
             }
         }
         return grid;
     }
 
 
-    public VBox getDegreePlanCoursesPane() throws IOException{
+    public VBox getDataTitleInPane(String path, String title) throws IOException{
         VBox pane = new VBox(); //The pane is placing its children panes vertically
         pane.setSpacing(50);
 
         HBox degreePlanTitlePane = new HBox(); //Title
         degreePlanTitlePane.setAlignment(Pos.BASELINE_CENTER);
         degreePlanTitlePane.setPadding(new Insets(50,0,15,0));
-        Label degreePlanTitle = new Label("Degree Plan Courses");
+        Label degreePlanTitle = new Label(title);
         degreePlanTitle.setFont(Font.font ("Verdana", 20));
         degreePlanTitlePane.getChildren().add(degreePlanTitle);
 
-        GridPane planCoursesPane = getDataInGridPane("src/main/java/com/example/termproject/data/DegreePlan.csv");
+        GridPane planCoursesPane = getDataInGridPane(path);
 
         pane.getChildren().add(degreePlanTitlePane); //adding the title to the pane 
         pane.getChildren().add(planCoursesPane); //adding the list of plan courses to the pane
 
         return pane;
     }
+
+    class Basket {
+        private ArrayList<Section> sections = new ArrayList<>();
+
+        public void addSection(Section section) {
+            sections.add(section);
+        }
+        
+        public ArrayList<Section> getSections() {
+            return this.sections;
+        }
+    }
+
+    class AddHandlerClass implements EventHandler<ActionEvent> {
+        public void handle(ActionEvent e) {
+            Button numberButton = (Button) e.getTarget();
+            numberButton.setDisable(true);
+            (removeButtons.get(Integer.parseInt(numberButton.getId()) - 1)).setDisable(false); //Enabling the remove
+
+            System.out.println("ADD " + numberButton.getId()); //Add the section to the basket
+        }
+    }
+
+    class RemoveHandlerClass implements EventHandler<ActionEvent> {
+        public void handle(ActionEvent e) {
+            Button numberButton = (Button) e.getTarget();
+            numberButton.setDisable(true);
+            (addButtons.get(Integer.parseInt(numberButton.getId()) - 1)).setDisable(false); //Enabling the add
+
+            System.out.println("Remove " + numberButton.getId()); //Remove the section from the basket
+        }
+    }
 }
-
-
-//Working with Buttons.......
-// Button btn = new Button("Change");
-// ChangeHandlerClass handler1 = new ChangeHandlerClass();
-// btn.setOnAction(handler1);
-// mainPane.getChildren().add(btn);
-
-// class ChangeHandlerClass implements EventHandler<ActionEvent> {
-//     public void handle(ActionEvent e) {
-//         Pane pane = new Pane();
-//         Label l = new Label("new page");
-//         l.setFont(Font.font ("Verdana", 35));
-//         pane.getChildren().add(l);
-//         Scene scene = new Scene(pane, 1400,720);
-//         System.out.println("clicked");
-//         stage.setScene(scene);
-//     }
-// }
