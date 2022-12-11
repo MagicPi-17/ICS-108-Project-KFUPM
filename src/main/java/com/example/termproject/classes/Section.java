490c8a6240e92d99fc4fd8537738cca385d537a7
MagicPi-17 <9martada9@gmail.com>
2022-12-11 20:40:26 +0300

    creating section class and it reader

--- src/main/java/com/example/termproject/classes/Section.java
+++ src/main/java/com/example/termproject/classes/Section.java
@@ -1,46 +1,94 @@
 package com.example.termproject.classes;
+
+
+// Course-Sec,Activity,CRN,Course Name,Instructor,Day,Time,Location,Status,Waitlist
 public class Section {
-    Course course;
-    Timing time;
+    String course_section;
+    String activity;
+    String CRN;
+    String courseName;
+    String instructor;
+    String day;
+    String time;
     String location;
+    String status;
+    String waitlist;
 
-    Section(Course course, Timing time, String location) {
-        this.course = course;
+    public Section(String course_section, String Activity, String CRN,
+                   String courseName, String Instructor, String day,
+                   String time, String Location, String status, String waitlist){
+        this.course_section = course_section;
+        this.activity = Activity;
+        this.CRN = CRN;
+        this.courseName = courseName;
+        this.instructor = Instructor;
+        this.day = day;
         this.time = time;
-        this.location = location;
+        this.location = Location;
+        this.status = status;
+        this.waitlist = waitlist;
     }
 
-    public Course getCourse() {
-        return this.course;
+
+    public String getActivity() {
+        return activity;
     }
-    public Timing getTime() {
-        return this.time;
+
+    public String getCourse_section() {
+        return course_section;
     }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getCRN() {
+        return CRN;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
     public String getLocation() {
-        return this.location;
+        return location;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getWaitlist() {
+        return waitlist;
+    }
+
+    @Override
+    public String toString() {
+        return "Section{" +
+                "course_section='" + course_section + '\'' +
+                ", Activity='" + activity + '\'' +
+                ", CRN='" + CRN + '\'' +
+                ", courseName='" + courseName + '\'' +
+                ", Instructor='" + instructor + '\'' +
+                ", day='" + day + '\'' +
+                ", time='" + time + '\'' +
+                ", Location='" + location + '\'' +
+                ", status='" + status + '\'' +
+                ", waitlist='" + waitlist + '\'' +
+                '}';
+    }
+
+    public Section(String[] data) {
+        this(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9]);
     }
 
 }
 
-
-class Timing {
-    int start;
-    int end;
-    String days;
-
-    Timing(int start, int end, String days) {
-        this.start = start;
-        this.end = end;
-        this.days = days;
-    }
-
-    public int getStart() {
-        return this.start;
-    }
-    public int getEnd() {
-        return this.end;
-    }
-    public String getDays() {
-        return this.days;
-    }
-}
--- src/main/java/com/example/termproject/readWriteDataClasses/DataReader.java
+++ src/main/java/com/example/termproject/readWriteDataClasses/DataReader.java
@@ -1,9 +1,11 @@
 package com.example.termproject.readWriteDataClasses;
 
 import com.example.termproject.classes.CSVReader;
+import com.example.termproject.classes.Section;
 import com.example.termproject.classes.Student;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class DataReader {
 
@@ -12,8 +14,27 @@ public class DataReader {
         CSVReader cvsReader = new CSVReader("src/main/java/com/example/termproject/data/FinishedCourses.csv");
         for (String[] finishedCourseData : cvsReader.readTo2DArray()) {
             newStudent.addFinishedCourse(finishedCourseData);
-        }
+            }
         return newStudent;
+        }
 
+    public static ArrayList<Section> getCourseOffering() throws FileNotFoundException {
+        ArrayList<Section> sections = new ArrayList<Section>();
+        CSVReader cvsReader = new CSVReader("src/main/java/com/example/termproject/data/CourseOffering.csv");
+        for (String[] section : cvsReader.readTo2DArray()) {
+            sections.add(new Section(section));
+        }
+        sections.remove(0);
+        return sections;
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            ArrayList<Section> sections = getCourseOffering();
+            System.out.println(sections.get(0).toString());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
