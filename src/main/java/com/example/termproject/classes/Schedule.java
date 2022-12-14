28500680b089a3bb7fe69ab3dc277710c3ccd522
MagicPi-17 <9martada9@gmail.com>
2022-12-14 10:18:54 +0300

    update in schedule class

--- src/main/java/com/example/termproject/classes/Schedule.java
+++ src/main/java/com/example/termproject/classes/Schedule.java
@@ -4,24 +4,75 @@ import com.example.termproject.readWriteDataClasses.DataReader;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+
+class SectionComparator implements Comparator<Section> {
+    @Override
+    public int compare(Section o1, Section o2) {
+        int time1 = Integer.parseInt(o1.getTime().split("-")[0]);
+        int time2 = Integer.parseInt(o2.getTime().split("-")[0]);
+        if(time1 > time2) {return 1;}
+        else if(time1 == time2) {return 0;}
+        else {return -1;}
+    }
+}
 
 public class Schedule {
-    ArrayList<Section> sections_basket;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        new Schedule(DataReader.getAllowedSections(), "221");
+
+    }
+    ArrayList<Section> sections;
+    TreeMap<Section, String> sectionsDays = new TreeMap<>(new SectionComparator());
+    ArrayList<Section> sundaySections = new ArrayList<Section>();
+    ArrayList<Section> MondaySections = new ArrayList<Section>();
+    ArrayList<Section> TuesSections = new ArrayList<Section>();
+    ArrayList<Section> wednesdaySections = new ArrayList<Section>();
+    ArrayList<Section> ThursdaySections = new ArrayList<Section>();
     String term;
 
     public Schedule() {
-        sections_basket = new ArrayList<Section>();
+        sections = new ArrayList<Section>();
         term = "";}
 
-    public ArrayList<Section> getSections_basket() {return sections_basket;}
+    public void addSectionToDays(Section section) {
+        String days = section.getDay();
+        Character day;
+        for(int i = 0; i < days.length();i++) {
+            day = days.charAt(i);
+            if(day == 'U') {}
+            else if(day == 'M') {}
+            else if(day == 'T') {}
+            else if(day == 'W') {}
+            else if(day == 'R') {}
+
+        }
+    }
+    public Boolean checkTime(Section section) {
+        sectionsDays.put(sections.get(0), sections.get(0).getDay());
+        sectionsDays.put(sections.get(1), sections.get(1).getDay());
+        sectionsDays.put(sections.get(2), sections.get(2).getDay());
+
+        for(Section s : sectionsDays.keySet()) {
+            System.out.println(section.toString());
+        }
+
+
+
+        return true;
+    }
+
+    public ArrayList<Section> getSections() {return sections;}
     public String getTerm() {return term;}
-    public void setSections_basket(ArrayList<Section> sections_basket) {this.sections_basket = sections_basket;}
+    public void setSections(ArrayList<Section> sections) {this.sections = sections;}
     public void setTerm(String term) {this.term = term;}
 
     public Schedule(ArrayList<Section> sections_basket, String term)  {
-        this.sections_basket = sections_basket;
+        this.sections = sections_basket;
         this.term = term;
     }
 
--- src/main/java/com/example/termproject/classes/Section.java
+++ src/main/java/com/example/termproject/classes/Section.java
@@ -1,8 +1,14 @@
 package com.example.termproject.classes;
 
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 // Course-Sec,Activity,CRN,Course Name,Instructor,Day,Time,Location,Status,Waitlist
-public class Section {
+
+
+
+public class Section implements Comparable<Section> {
     String course_section;
     String activity;
     String CRN;
@@ -27,6 +33,42 @@ public class Section {
         this.location = Location;
         this.status = status;
         this.waitlist = waitlist;
+    }
+
+    public static void main(String[] args) {
+        String[] strings1 = "AE  540-01,LEC,23978,Flight Dynamics and Control I,Ayman Abdallah,UT,0800-0900,63-024,Open,Closed".split(",");
+        Section section = new Section(strings1);
+        String[] strings2 = "AE  540-01,LEC,23978,Flight Dynamics and Control I,Ayman Abdallah,UT,0730-0830,63-024,Open,Closed".split(",");
+        Section section1 = new Section(strings2);
+
+        int result = section1.getTimeDifference(section);
+        System.out.println(result);
+    }
+
+
+    // this function return the time between two section and if they two time wrong the function return -1
+    public int getTimeDifference(Section section) {
+        String[] times2 = time.split("-");
+        String[] times1 = section.getTime().split("-");
+
+        int startTime1 = Integer.valueOf(times1[0]);
+        int startTime2 = Integer.valueOf(times2[0]);
+        int endTime1 = Integer.valueOf(times1[1]);
+        int endTime2 = Integer.valueOf(times2[1]);
+
+        int hoursBigGap = (endTime2/100) - (startTime1/100) - 1;
+        int hoursSmallGap = (startTime2/100) - (endTime1/100) - 1;
+        int minutesBigGap = 60 + (endTime2 % 100) - (startTime1 % 100);
+        int minutesSmallGap = 60 + (startTime2 % 100) - (endTime1 % 100);
+
+        int dif1 = hoursBigGap * 60 + minutesBigGap;
+        int dif2 = hoursSmallGap * 60 + minutesSmallGap;
+
+        int sameSignCheck = Integer.signum(dif1) * Integer.signum(dif2);
+        if(sameSignCheck == 1 || sameSignCheck == 0) {
+            return (Math.abs(dif1) > Math.abs(dif2)) ? Math.abs(dif2): Math.abs(dif1);
+        }
+        else {return -1;}
     }
 
 
@@ -90,5 +132,13 @@ public class Section {
         this(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9]);
     }
 
+    @Override
+    public int compareTo(Section o) {
+        int time1 = Integer.parseInt(time.split("-")[0]);
+        int time2 = Integer.parseInt(o.getTime().split("-")[0]);
+        if(time1 > time2) {return 1;}
+        else if(time1 == time2) {return 0;}
+        else {return -1;}
+    }
 }
 
